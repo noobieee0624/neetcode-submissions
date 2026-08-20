88adc9d258f930a65a7560f7a1095b88d4ad3997
Thomas Liu <thomasliu0724@gmail.com>
2026-08-20 10:36:23 -0500

    Add: validate-parentheses - submission-8

---
+++ Data Structures & Algorithms/validate-parentheses/submission-8.py
@@ -0,0 +1,26 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        key = {
+            "(": ")",
+            "[": "]",
+            "{": "}",
+        }
+        stack = []
+
+        for char in s:
+            if char in key:
+                stack.append(key[char])
+            elif not stack:
+                return False
+            else:
+                if char != stack.pop():
+                    return False
+
+        if stack:
+            return False
+
+        return True
+            
+
+
+
